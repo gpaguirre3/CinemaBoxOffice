fb70b2a36cb32410917bb7428f81d5a0135cbb43
Jhoel-Chicaiza7 <jhoel.david852456@gmail.com>
2020-06-24 23:44:06 -0500

    Cinema Box Office v1.3.3

--- 06Code/POO6382-CinemaBoxOffice/src/ec/edu/espe/cinemaboxoffice/model/Payment.java
+++ 06Code/POO6382-CinemaBoxOffice/src/ec/edu/espe/cinemaboxoffice/model/Payment.java
@@ -5,6 +5,7 @@
  */
 package ec.edu.espe.cinemaboxoffice.model;
 
+import java.util.Scanner;
 /**
  *
  * @author Kevin Chuquimarca ESPE-DCCO
@@ -12,13 +13,59 @@ package ec.edu.espe.cinemaboxoffice.model;
 public class Payment {
     private Customer customer;
     private String card;
+
+    public Payment(Customer customer, String card) {
+        this.customer = customer;
+        this.card = card;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public String getCard() {
+        return card;
+    }
+
+    public void setCard(String card) {
+        this.card = card;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" + "customer=" + customer + ", card=" + card + '}';
+    }
+    
     
     
     public void pay(){
+        Scanner scanner = new Scanner(System.in);
         
+        
+        System.out.println();
+        System.out.println("Enter your card details ");
+        System.out.println("Enter your id:  ");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+        
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        
+        System.out.println("Enter your age:  ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+        
+        System.out.print("Enter number of the card: ");
+        String card = scanner.nextLine();
+        
+        System.out.println("\n your card information was entered! ");
     }
     
     public void cancelPay(){
-        
+                       
     }
 }
