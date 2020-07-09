ccc28b78c99eebda0ecc42e779e1a6bee2418783
alissonclavijo <anclavijo@espe.edu.ec>
2020-07-08 19:49:59 -0500

    JSON

---
+++ assignments/Clavijo/19JSON/promotion.java
@@ -0,0 +1,9 @@
+public class Promotion{
+	private String promotionkey;
+	private String promotionName;
+	private int promotionDay;
+	private float foodPricePromotion;
+
+//TODO Constructor, setters and getters
+
+}
