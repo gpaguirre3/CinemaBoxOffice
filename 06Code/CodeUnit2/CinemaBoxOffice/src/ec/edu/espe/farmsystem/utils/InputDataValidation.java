45f81cc46c1c37458364a8cbc911fe0e6e0fc98e
gpaguirre3 <gabrielaguirre1104@hotmail.com>
2020-07-25 23:28:49 -0500

    updateValidateIDandCard

--- 06Code/CodeUnit2/CinemaBoxOffice/src/ec/edu/espe/farmsystem/utils/InputDataValidation.java
+++ 06Code/CodeUnit2/CinemaBoxOffice/src/ec/edu/espe/farmsystem/utils/InputDataValidation.java
@@ -62,4 +62,56 @@ public class InputDataValidation {
         String data = string.nextLine();
         return Float.parseFloat(data);
     }
+    
+    public static boolean checkCard(String card) {
+        if (card.length() != 16) {
+            return false;
+        }
+        int number = 0, sum = 0;
+        for (int i = 0; i < 16; i++) {
+            number = Character.getNumericValue(card.charAt(i));
+            if (i % 2 == 0) {
+                number *= 2;
+                if(number > 9){
+                    number=number-9;                
+                }
+            }
+            sum = sum + number;
+        }
+        if (sum % 10 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean validateCI(String CI) {
+        if (CI.length() != 10) {
+            return false;
+        }
+        for (int i = 0; i < 10; i++) {
+            if (!Character.isDigit(CI.charAt(i))) {
+                return false;
+            }
+        }
+        int lastDigit = Character.getNumericValue((CI.charAt(CI.length() - 1)));
+        int digit,sumPairs = 0, oddSum = 0;
+
+        for (int i = 0; i < 9; i++) {
+            digit = Character.getNumericValue(CI.charAt(i));
+
+            if ((i + 1) % 2 == 0) {
+                sumPairs += digit;
+            } else {
+                oddSum += (digit * 2) > 9 ? (digit * 2) - 9 : (digit * 2);
+            }
+        }
+        int total = sumPairs + oddSum;
+        int higher = (10 - (total % 10)) + total;
+
+        if ((total % 10) == 0) {
+            return lastDigit == 0;
+        }
+        return lastDigit == (higher - total);
+    }
 }
