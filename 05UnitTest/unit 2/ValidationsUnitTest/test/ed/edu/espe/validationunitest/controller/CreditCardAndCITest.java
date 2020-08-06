d6c82d02806a1d0899773fe78595d552fd2b88bb
gpaguirre3 <gabrielaguirre1104@hotmail.com>
2020-08-06 03:19:32 -0500

    UnitTest Validation CI And CreditCard

---
+++ 05UnitTest/unit 2/ValidationsUnitTest/test/ed/edu/espe/validationunitest/controller/CreditCardAndCITest.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ed.edu.espe.validationunitest.controller;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Gabriel Aguirre
+ */
+public class CreditCardAndCITest {
+    
+    public CreditCardAndCITest() {
+    }
+
+    /**
+     * Test of checkCard method, of class CreditCardAndCI.
+     */
+    @Test
+    public void testCheckCard1() {
+        System.out.println("checkCard");
+        String card = "4048598412673666";
+        boolean expResult = true;
+        boolean result = CreditCardAndCI.checkCard(card);
+        assertEquals(expResult, result);
+        
+    }
+    public void testCheckCard2() {
+        System.out.println("checkCard");
+        String card = "4608003816501748";
+        boolean expResult = true;
+        boolean result = CreditCardAndCI.checkCard(card);
+        assertEquals(expResult, result);
+    }
+    public void testCheckCard3() {
+        System.out.println("checkCard");
+        String card = "4618681820176819";
+        boolean expResult = true;
+        boolean result = CreditCardAndCI.checkCard(card);
+        assertEquals(expResult, result);
+    }
+    public void testCheckCard4() {
+        System.out.println("checkCard");
+        String card = "4835774663674896";
+        boolean expResult = true;
+        boolean result = CreditCardAndCI.checkCard(card);
+        assertEquals(expResult, result);
+    }
+    public void testCheckCard5() {
+        System.out.println("checkCard");
+        String card = "4528804794441477";
+        boolean expResult = true;
+        boolean result = CreditCardAndCI.checkCard(card);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of validateCI method, of class CreditCardAndCI.
+     */
+    @Test
+    public void testValidateCI1() {
+        System.out.println("validateCI");
+        String CI = "1727620054";
+        boolean expResult = true;
+        boolean result = CreditCardAndCI.validateCI(CI);
+        assertEquals(expResult, result);
+    }
+    public void testValidateCI2() {
+        System.out.println("validateCI");
+        String CI = "1721482998";
+        boolean expResult = true;
+        boolean result = CreditCardAndCI.validateCI(CI);
+        assertEquals(expResult, result);
+    }
+    public void testValidateCI3() {
+        System.out.println("validateCI");
+        String CI = "1751364819";
+        boolean expResult = true;
+        boolean result = CreditCardAndCI.validateCI(CI);
+        assertEquals(expResult, result);
+    }
+    public void testValidateCI4() {
+        System.out.println("validateCI");
+        String CI = "0952465334";
+        boolean expResult = true;
+        boolean result = CreditCardAndCI.validateCI(CI);
+        assertEquals(expResult, result);
+    }
+    public void testValidateCI5() {
+        System.out.println("validateCI");
+        String CI = "1708740962";
+        boolean expResult = true;
+        boolean result = CreditCardAndCI.validateCI(CI);
+        assertEquals(expResult, result);
+    }
+}
