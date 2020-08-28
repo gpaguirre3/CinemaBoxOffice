61c627ce0e43450eef0c2c66941bb4238a7d973c
alissonclavijo <anclavijo@espe.edu.ec>
2020-08-27 22:15:10 -0500

    abstract class Ticket

--- 06Code/CodeUnit2/CinemaBoxOffice/src/ec/edu/espe/cinemaboxoffice/model/Ticket.java
+++ 06Code/CodeUnit2/CinemaBoxOffice/src/ec/edu/espe/cinemaboxoffice/model/Ticket.java
@@ -9,12 +9,26 @@ package ec.edu.espe.cinemaboxoffice.model;
  *
  * @author Kevin Chuquimarca ESPE-DCCO
  */
-public class Ticket {
+public abstract class  Ticket {
+
+    protected String gender;
+    protected String title;
+    protected String ageRestriction;
+    protected int duration;
+    protected String namePoster;
     private int code;
     private MovieBillboard movie;
+
+    public Ticket(String gender, String title, String ageRestriction, int duration, String namePoster) {
+        this.gender = gender;
+        this.title = title;
+        this.ageRestriction = ageRestriction;
+        this.duration = duration;
+        this.namePoster = namePoster;
+    }
 
     public Ticket(int code, MovieBillboard movie) {
         this.code = code;
         this.movie = movie;
-    }
+    }  
 }
